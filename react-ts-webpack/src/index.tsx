ca48f86d111f62f4234af81e866d829e821428db
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-05-31 23:25:42 +0200

    upd

--- react-ts-webpack/src/index.tsx
+++ react-ts-webpack/src/index.tsx
@@ -1,14 +1,20 @@
 import React, {Fragment, useState} from 'react'
 import ReactDOM from 'react-dom'
 
+type FormElem = React.FormEvent<HTMLFormElement>
+
 export default function App(): JSX.Element {
-    const [value, setValue] = useState('');
-    debugger;
+    const [value, setValue] = useState<string>('');
+
+    const handleSubmit = (e: FormElem): void => {
+        e.preventDefault();
+        setValue('')
+    };
     return (
         <Fragment>
             <h1>Todo List</h1>
-            <form>
-                <input type='text'/>
+            <form onSubmit={handleSubmit}>
+                <input type='text' value={value} onChange={e => setValue(e.target.value)} required/>
                 <button type = 'submit'> Add Todo</button>
             </form>
         </Fragment>
