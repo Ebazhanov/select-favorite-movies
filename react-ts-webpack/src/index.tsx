99aa6a8c2c2aacf52922fdb79bc945dd0c18e1a0
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-05-31 23:02:20 +0200

    upd

--- react-ts-webpack/src/index.tsx
+++ react-ts-webpack/src/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, {Fragment, useState} from 'react'
 import ReactDOM from 'react-dom'
 
 export default function App(): JSX.Element {
+    const [value, setValue] = useState('');
+    debugger;
     return (
-        <div>
-            Hello!!
-        </div>
+        <Fragment>
+            <h1>Todo List</h1>
+            <form>
+                <input type='text'/>
+                <button type = 'submit'> Add Todo</button>
+            </form>
+        </Fragment>
     )
 }
 
