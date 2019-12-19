3f2921d275d26f3d43e062ba0dcd1ea5ec5a9687
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-12-19 17:14:27 +0100

    - next lesson 'add css'

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import {Store} from "./Store";
 
+interface IEpisode {
+    airdate: string
+    airstamp: string
+    airtime: string
+    id: number
+    image: {medium: string, original: string}
+    name: string
+    number: number
+    runtime: 30
+    season: 1
+    summary: string
+    url: string
+}
+
 export default function App(): JSX.Element {
     const {state, dispatch} = React.useContext(Store);
 
@@ -18,13 +32,25 @@ export default function App(): JSX.Element {
         })
     };
 
-    console.log(state);
-
     return (
         <React.Fragment>
+            <header className="header">
             <h1>Rick and Morty</h1>
             <p>Pick your favorite episode !!!</p>
+            </header>
+            <section className="episode-layout">
+                {state.episodes.map((episode: IEpisode) => {
+                    return (
+                        <section key = {episode.id} className="episode-box">
+                            <img src = {episode.image.medium} alt={`Rick and Mort' ${episode.name}`} />
+                            <div>{episode.name}</div>
+                            <section>
+                                Seasion: {episode.season} Number: {episode.number}
+                            </section>
+                        </section>
+                    )
+                })}
+            </section>
         </React.Fragment>
     )
-
 }
