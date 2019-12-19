0678a6bf434c2d21e67f875945a0837d23fc45b1
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-12-19 13:47:14 +0100

    - upd

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+
+  return (
+      <React.Fragment>
+        <h1>Rick and Morty</h1>
+      </React.Fragment>
+  )
+
+}
