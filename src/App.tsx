b049312178fd8dc7e140d6fdc824c77a614a9107
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-12-19 16:46:02 +0100

    - next lesson 'fetchDataAction'

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,26 @@ import React from 'react';
 import {Store} from "./Store";
 
 export default function App(): JSX.Element {
-    const store = React.useContext(Store);
+    const {state, dispatch} = React.useContext(Store);
+
+    React.useEffect(() => {
+        state.episodes.length === 0 && fetchDataAction()
+    });
+
+    const fetchDataAction = async () => {
+        const URL = 'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes';
+        const data = await fetch(URL);
+        const dataJSON = await data.json();
+        return dispatch({
+            type: 'FETCH_DATA',
+            payload: dataJSON._embedded.episodes
+        })
+    };
+
+    console.log(state);
+
     return (
         <React.Fragment>
-            {console.log(store)}
             <h1>Rick and Morty</h1>
             <p>Pick your favorite episode !!!</p>
         </React.Fragment>
--- src/Store.tsx
+++ src/Store.tsx
@@ -5,17 +5,28 @@ interface IState {
     favorites: []
 }
 
+interface IAction {
+    type: string;
+    payload: any;
+}
+
 const initialState: IState = {
     episodes: [],
     favorites: []
 };
 
-export const Store = React.createContext<IState>(initialState);
+export const Store = React.createContext<IState | any>(initialState);
 
-/*function reducer() {
-    // pass
-}*/
+function reducer(state: IState, action: IAction): IState {
+    switch (action.type) {
+        case 'FETCH_DATA':
+            return {...state, episodes: action.payload};
+        default:
+            return state;
+    }
+}
 
 export function StoreProvider(props: any): JSX.Element {
-    return <Store.Provider value={initialState}> {props.children}</Store.Provider>
+    const [state, dispatch] = React.useReducer(reducer, initialState)
+    return <Store.Provider value={{state, dispatch}}> {props.children}</Store.Provider>
 }
