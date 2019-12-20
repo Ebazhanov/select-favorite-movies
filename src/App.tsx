c8f83a8974b35bc1e8560d2261bedbc47ab018aa
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-12-20 14:29:48 +0100

    - add counter for favorites

--- src/App.tsx
+++ src/App.tsx
@@ -19,26 +19,49 @@ export default function App(): JSX.Element {
         })
     };
 
-    const toggleFavAction = (episode: IEpisode): IAction => dispatch({
-        type: 'ADD_FAV',
-        payload: episode
-    });
+    const toggleFavAction = (episode: IEpisode): IAction => {
+        const episodeInFav = state.favorites.includes(episode);
+        let dispatchObj = {
+            type: 'ADD_FAV',
+            payload: episode
+        };
+        if (episodeInFav) {
+            const favWithoutEpisode = state.favorites.filter((fav: IEpisode) => fav.id !== episode.id);
+            dispatchObj = {
+                type: 'REMOVE_FAV',
+                payload: favWithoutEpisode
+            }
+        }
+        return dispatch(dispatchObj);
+    };
 
     return (
         <React.Fragment>
             <header className="header">
+                <div>
                 <h1>Love Stories </h1>
                 <p>Pick your favorite episode !!!</p>
+                </div>
+                <div>
+                    Favorite(s): {state.favorites.length}
+                </div>
             </header>
             <section className="episode-layout">
                 {state.episodes.map((episode: IEpisode) => {
                     return (
-                        <section key={episode.id} className="episode-box">
-                            <img src={episode.image.medium} alt={`Love Stories' ${episode.name}`}/>
+                        <section key={episode.id}
+                                 className="episode-box">
+                            <img src={episode.image.medium}
+                                 alt={`Love Stories' ${episode.name}`}/>
                             <div>{episode.name}</div>
                             <section>
-                                <div>Season: {episode.season} Number: {episode.number}</div>
-                                <button type="button" onClick={() => toggleFavAction(episode)}>Favorites</button>
+                                <div>
+                                    Season: {episode.season}
+                                    Number: {episode.number}</div>
+                                <button type="button"
+                                        onClick={() => toggleFavAction(episode)}>
+                                    {state.favorites.find((fav: IEpisode) => fav.id === episode.id) ? 'Unfav' : 'Fav'}
+                                </button>
                             </section>
                         </section>
                     )
--- src/Store.tsx
+++ src/Store.tsx
@@ -14,6 +14,8 @@ function reducer(state: IState, action: IAction): IState {
             return {...state, episodes: action.payload};
         case 'ADD_FAV':
             return {...state, favorites: [...state.favorites, action.payload]};
+        case 'REMOVE_FAV':
+            return {...state, favorites: action.payload};
         default:
             return state;
     }
